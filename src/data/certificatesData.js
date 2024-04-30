f59ed433e9b1ed3a9393f8ddaf4df775a4442d46
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2024-04-30 18:00:03 +0300

    Commit

--- src/data/certificatesData.js
+++ src/data/certificatesData.js
@@ -1,9 +1,11 @@
 import PrBasic from "../images/SoftUniProgramingBasic.jpeg";
+import SEEMOUS from "../images/SEEMOUS.png";
+import NSOM from "../images/NSOM.png";
 
 const certificatesData = [
     {
         id: 1,
-        image: PrBasic,
+        image: SEEMOUS,
         from: "SEEMOUS",
         description: `A certificate for participation in the international awareness of mathematics in the balkans - SEEMOUS. 
                         SEEMOUS is a regional mathematical competition for university-level students from Southeastern European countries.
@@ -11,7 +13,7 @@ const certificatesData = [
     },
     {
         id: 2,
-        image: PrBasic,
+        image: NSOM,
         from: "NSOM",
         description: `A certificate for participation in national student olympiad in mathematics 2023. NSOM is a competition for university students in a 
                     country designed to encourage mathematical creativity and develop mathematical skills among young people. The competition was held on 12.05.2023.`
